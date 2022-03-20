6883fd76829ee596103d4d7150ff45e8b774aa03
Primahale <mahalepriyanka82@gmail.com>
2022-03-20 14:19:54 +0530

    a

---
+++ express Validation/src/controllers/user.controllers.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/user.models");
+
+const router = express.Router();
+
+router.post(
+  "/",
+  // body('username').isEmail(),
+  body("firstName")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("First Name cannot be empty"),
+    body("lastName")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("last Name cannot be empty"),
+    body("email")
+    .isEmail()
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+
+      if (user) {
+        throw new Error("Email is already taken");
+      }
+      return true;
+    }),
+    body("pincode")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("pincode cannot be empty")
+    .isLength({min:6})
+    .withMessage("pincode must be 6 characters"),
+    body("age")
+    .not()
+    .isEmpty()
+    .withMessage("Age cannot be empty")
+    .isNumeric()
+    .withMessage("Age must be a number between 1 and 100")
+    .custom((val) => {
+      if (val < 1 || val > 100) {
+        throw new Error("Incorrect age provided");
+      }
+      return true;
+    }),
+ 
+ 
+  async (req, res) => {
+    try {
+      console.log(body("firstName"));
+      const errors = validationResult(req);
+      console.log({ errors });
+      if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() });
+      }
+
+      const user = await User.create(req.body);
+
+      return res.status(201).send(user);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  }
+);
+
+module.exports = router;
+
