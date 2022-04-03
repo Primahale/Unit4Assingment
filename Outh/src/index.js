96bffb581d1129d51108c23bc7981427a78f6b07
Primahale <mahalepriyanka82@gmail.com>
2022-04-03 20:37:41 +0530

    Outh

--- Outh/src/index.js
+++ Outh/src/index.js
@@ -1,14 +1,43 @@
 const express = require("express");
+const connect = require("./configs/google-oauth");
+const userController = require("./controllers/user.controller")
+const productController = require("./controllers/product.controller")
 
+const {register,login, generateToken} = require("./controllers/auth.controller")
 const app = express();
+const passport = require("./configs/google-oauth")
+
 
 app.use(express.json());
 
-app.listen(5000,async ()=> {
+
+app.use("/users", userController)
+
+app.post("/register", register)
+
+app.post("/login", login)
+
+app.use("/products", productController)
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+ 
+app.get(
+'/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login', session:false } ),
+
+  function(req, res) {
+    const token = generateToken(req.user)
+    return res.status(200).send({user:req.user, token})
+  }
+)
+
+app.listen(5000, async () => {
     try{
-        console.log("on port 5000");
+        // await connect();
+        console.log("listening on port 5000")
     }
     catch(err){
         console.log(err.message);
     }
-})
+});
